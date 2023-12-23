0bdb912eea6bf323cf3dd7b7de6928b06c62c32d
Matteo Pietro Dazzi <matteopietro.dazzi@gmail.com>
2023-12-23 12:45:07 +0100

    feat: refetch interval

--- packages/client/src/components/DownloadList.tsx
+++ packages/client/src/components/DownloadList.tsx
@@ -5,8 +5,10 @@ import { getDownloads } from '../services/downloads'
 import { DownloadableItem } from './DownloadableItem/DownloadableItem'
 import { ErrorBoundary } from './ErrorBoundary'
 
+const REFETCH_INTERVAL = 5_000
+
 export const DownloadList = () => {
-    const { data = [], isLoading, isError } = useQuery({ queryKey: ['downloads'], queryFn: getDownloads })
+    const { data = [], isLoading, isError } = useQuery({ queryKey: ['downloads'], queryFn: getDownloads, refetchInterval: REFETCH_INTERVAL })
 
     return <ErrorBoundary isLoading={isLoading} isError={isError}>
         <DataView value={data} itemTemplate={DownloadableItem} />
