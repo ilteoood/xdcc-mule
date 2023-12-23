3cb0e8c209c2013923e9fa925f256d6a18deb37f
Matteo Pietro Dazzi <matteopietro.dazzi@gmail.com>
2023-12-23 11:07:54 +0100

    feat: first client commit

---
+++ packages/client/vite.config.ts
@@ -0,0 +1,14 @@
+import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000'
+      }
+    }
+  }
+})
