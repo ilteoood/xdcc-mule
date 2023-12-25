19774464ca9b41a0e73ce687c59c3a7b2cc17ecb
Matteo Pietro Dazzi <matteopietro.dazzi@gmail.com>
2023-12-25 18:09:19 +0100

    Update config.ts

--- packages/server/src/utils/config.ts
+++ packages/server/src/utils/config.ts
@@ -1,5 +1,6 @@
 export const config = {
 	databaseUrl: process.env.DATABASE_URL,
 	nickname: process.env.NICKNAME || "xdcc-mule",
-	downloadPath: process.env.DOWNLOAD_PATH || "./",
+ downloadPath: process.env.DOWNLOAD_PATH || "./",
+ port: process.env.PORT || 3000
 };
