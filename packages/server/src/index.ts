c105a1a4bfb7680ed7805622c67e34a0e0bd6c8f
Matteo Pietro Dazzi <matteopietro.dazzi@gmail.com>
2024-10-19 20:42:20 +0200

    chore: cron

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,7 +1,7 @@
 import fastifyStatic from "@fastify/static";
-import { fastifyCronJob } from '@kakang/fastify-cronjob';
 import { dirname } from "desm";
 import fastify from "fastify";
+import cron from 'node-cron';
 import { join } from "node:path";
 import apiController from "./routes/api.js";
 import { config } from "./utils/config.js";
@@ -17,8 +17,6 @@ app.register(fastifyStatic, {
 	root: join(dirname(import.meta.url), "public"),
 });
 
-app.register(fastifyCronJob);
-
-app.cronjob.setCronJob(refresh, "0 * * * *", 'async')
-
-app.listen({ port: config.port, host: "::" });
+app.listen({ port: config.port, host: "::" }, () => {
+	cron.schedule('0 * * * *', refresh);
+});
